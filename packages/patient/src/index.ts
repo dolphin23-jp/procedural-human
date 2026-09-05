1a575b3846e9694421f81d564dd61c68303667b5
dolphin23-jp <h062.dolphin23@gmail.com>
2026-09-06 08:23:29 +0900

    fix: move runtime medical state under patient ownership

--- packages/patient/src/index.ts
+++ packages/patient/src/index.ts
@@ -23,10 +23,11 @@ export interface RuntimeMedicalState {
   readonly integrity: MedicalIntegrityState;
 }
 
-const structureState = new WeakMap<
-  PatientStructureInstance,
-  RuntimeMedicalState
->();
+const initialMedicalStateAccess = Symbol('initialMedicalStateAccess');
+const patientRuntimeStateAccess = Symbol('patientRuntimeStateAccess');
+
+const snapshot = (integrity: MedicalIntegrityState): RuntimeMedicalState =>
+  Object.freeze({ integrity });
 
 export interface PatientStructureDescriptor {
   readonly id: StructureId;
@@ -39,23 +40,62 @@ export class PatientStructureInstance {
   readonly id: StructureId;
   readonly canonicalEntityId: EntityId;
   readonly representationAssetIds: readonly AssetId[];
+  readonly #initialIntegrity: MedicalIntegrityState;
 
   constructor(descriptor: PatientStructureDescriptor) {
     this.id = descriptor.id;
     this.canonicalEntityId = descriptor.canonicalEntityId;
-    this.representationAssetIds = [...descriptor.representationAssetIds];
-    structureState.set(
-      this,
-      descriptor.initialMedicalState ?? { integrity: 'intact' },
+    this.representationAssetIds = Object.freeze([
+      ...descriptor.representationAssetIds,
+    ]);
+    this.#initialIntegrity = descriptor.initialMedicalState?.integrity ?? 'intact';
+    Object.freeze(this);
+  }
+
+  [initialMedicalStateAccess](): MedicalIntegrityState {
+    return this.#initialIntegrity;
+  }
+}
+
+class PatientRuntimeState {
+  readonly #integrityByStructure: Map<StructureId, MedicalIntegrityState>;
+
+  constructor(structures: readonly PatientStructureInstance[]) {
+    this.#integrityByStructure = new Map(
+      structures.map((structure) => [
+        structure.id,
+        structure[initialMedicalStateAccess](),
+      ]),
     );
   }
 
-  get medicalState(): RuntimeMedicalState {
-    const state = structureState.get(this);
-    if (!state) {
-      throw new Error(`Missing medical state for structure: ${this.id}`);
+  read(structureId: StructureId): RuntimeMedicalState {
+    const integrity = this.#integrityByStructure.get(structureId);
+    if (!integrity) {
+      throw new Error(`Unknown patient structure: ${structureId}`);
     }
-    return state;
+    return snapshot(integrity);
+  }
+
+  puncture(structureId: StructureId): StateTransitionResult {
+    const previousState = this.read(structureId);
+    if (previousState.integrity === 'punctured') {
+      return Object.freeze({
+        structureId,
+        previousState,
+        currentState: previousState,
+        changed: false,
+      });
+    }
+
+    this.#integrityByStructure.set(structureId, 'punctured');
+    const currentState = this.read(structureId);
+    return Object.freeze({
+      structureId,
+      previousState,
+      currentState,
+      changed: true,
+    });
   }
 }
 
@@ -75,27 +115,39 @@ export class PatientInstance {
   readonly morphology: PatientMorphology;
   readonly anatomy: PatientAnatomy;
   readonly #structures: ReadonlyMap<StructureId, PatientStructureInstance>;
+  readonly #runtimeState: PatientRuntimeState;
 
   constructor(descriptor: PatientInstanceDescriptor) {
     this.id = descriptor.id;
     this.morphology = descriptor.morphology;
-    this.anatomy = {
-      canonicalAnatomy: descriptor.anatomy.canonicalAnatomy,
-      structures: [...descriptor.anatomy.structures],
-    };
 
     const structures = new Map<StructureId, PatientStructureInstance>();
-    for (const structure of this.anatomy.structures) {
+    for (const structure of descriptor.anatomy.structures) {
       if (structures.has(structure.id)) {
         throw new Error(`Duplicate patient structure id: ${structure.id}`);
       }
       structures.set(structure.id, structure);
     }
+
+    const structureList = Object.freeze([...structures.values()]);
+    this.anatomy = Object.freeze({
+      canonicalAnatomy: Object.freeze({ ...descriptor.anatomy.canonicalAnatomy }),
+      structures: structureList,
+    });
     this.#structures = structures;
+    this.#runtimeState = new PatientRuntimeState(structureList);
   }
 
   structure(id: StructureId): PatientStructureInstance | undefined {
     return this.#structures.get(id);
+  }
+
+  medicalStateFor(id: StructureId): RuntimeMedicalState {
+    return this.#runtimeState.read(id);
+  }
+
+  [patientRuntimeStateAccess](): PatientRuntimeState {
+    return this.#runtimeState;
   }
 }
 
@@ -112,35 +164,13 @@ export interface StateTransitionResult {
 }
 
 export class PatientStateTransitionService {
-  readonly #patient: PatientInstance;
+  readonly #runtimeState: PatientRuntimeState;
 
   constructor(patient: PatientInstance) {
-    this.#patient = patient;
+    this.#runtimeState = patient[patientRuntimeStateAccess]();
   }
 
   apply(request: StateTransitionRequest): StateTransitionResult {
-    const structure = this.#patient.structure(request.structureId);
-    if (!structure) {
-      throw new Error(`Unknown patient structure: ${request.structureId}`);
-    }
-
-    const previousState = structure.medicalState;
-    if (previousState.integrity === 'punctured') {
-      return {
-        structureId: structure.id,
-        previousState,
-        currentState: previousState,
-        changed: false,
-      };
-    }
-
-    const currentState: RuntimeMedicalState = { integrity: 'punctured' };
-    structureState.set(structure, currentState);
-    return {
-      structureId: structure.id,
-      previousState,
-      currentState,
-      changed: true,
-    };
+    return this.#runtimeState.puncture(request.structureId);
   }
 }
