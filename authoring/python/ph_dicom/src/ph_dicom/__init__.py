4cb1944a013e8123b4dd4edd28472bb00ec84822
dolphin23-jp <h062.dolphin23@gmail.com>
2026-09-08 12:20:47 +0900

    TASK-A05: export source archive scaffold API

---
+++ authoring/python/ph_dicom/src/ph_dicom/__init__.py
@@ -0,0 +1,15 @@
+from .source_archive import (
+    AlignedSourceArchiveSpec,
+    SourceArchiveError,
+    acquire_archive,
+    scaffold_manifest,
+    write_manifest_scaffold,
+)
+
+__all__ = [
+    "AlignedSourceArchiveSpec",
+    "SourceArchiveError",
+    "acquire_archive",
+    "scaffold_manifest",
+    "write_manifest_scaffold",
+]
